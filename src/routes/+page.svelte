657f51e53146741673209c43c75cf65c4f85fecb
Szymeo <dev.sgracki@gmail.com>
2024-11-16 00:50:10 +0100

    :poodle: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "./global.css";
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<main class="w-full h-full text-white">
+  <div data-tauri-drag-region="true" class="draggable h-6 w-full"></div>
+</main>
