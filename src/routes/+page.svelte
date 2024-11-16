35f25f9de40404fff4561a6d3a8b8848eca1e0f0
Szymeo <dev.sgracki@gmail.com>
2024-11-16 01:47:08 +0100

    :ticket: eslint, prettier, routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-  import "./global.css";
-  import { invoke } from "@tauri-apps/api/core";
+	import ArrowLeft from '$lib/shared/icons/ArrowLeft.svelte';
+	import './global.css';
+	import { invoke } from '@tauri-apps/api/core';
 
-  let name = $state("");
-  let greetMsg = $state("");
+	let name = $state('');
+	let greetMsg = $state('');
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+	async function greet(event: Event) {
+		event.preventDefault();
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		greetMsg = await invoke('greet', { name });
+	}
 </script>
 
-<main class="w-full h-full text-white">
-  <div data-tauri-drag-region="true" class="draggable h-6 w-full"></div>
+<main class="w-full flex flex-col h-full text-white">
+	<div
+		data-tauri-drag-region="true"
+		class="draggable h-7 w-full px-20 flex items-center justify-between bg-gray-800"
+	></div>
+
+	<a href="/1">Project 1</a>
 </main>
---
+++ src/routes/[projectId]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import ArrowLeft from '$lib/shared/icons/ArrowLeft.svelte';
+</script>
+
+<main class="w-full flex flex-col h-full text-white">
+	<div
+		data-tauri-drag-region="true"
+		class="draggable h-7 w-full px-20 flex items-center justify-center bg-gray-800"
+	>
+		<button class="absolute left-20 flex items-center text-xs font-mono gap-1 text-gray-400">
+			<ArrowLeft stroke="stroke-gray-400" class="w-4 h-4" />
+			<span>back</span>
+		</button>
+
+		<span class="block text-xs text-gray-400 font-bold font-sans">Project 1</span>
+	</div>
+
+	<div class="w-full h-full flex flex-col px-3 py-3 gap-2">
+		<div class="w-full flex-1 flex items-center justify-center gap-2">
+			<div class="bg-gray-900 rounded-md w-full h-full"></div>
+			<div class="bg-gray-900 rounded-md w-full h-full"></div>
+		</div>
+
+		<div class="w-full flex-1 bg-gray-900 rounded-md"></div>
+	</div>
+</main>
